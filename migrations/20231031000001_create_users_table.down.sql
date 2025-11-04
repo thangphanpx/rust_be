3d8eeb0dfe81779a000e5d45a8cde2db4b53f71c
thangphanmycolor <thang.phan@mycolor.biz>
2025-11-04 13:48:23 +0700

    remove sqlx

--- migrations/20231031000001_create_users_table.down.sql
+++
@@ -1,6 +0,0 @@
--- Drop indexes first
-DROP INDEX IF EXISTS idx_users_username;
-DROP INDEX IF EXISTS idx_users_email;
-
--- Drop users table
-DROP TABLE IF EXISTS users;
--- migrations/20231031000003_seed_initial_data.down.sql
+++
@@ -1,3 +0,0 @@
--- Remove sample data
-DELETE FROM posts WHERE title IN ('Welcome to our API', 'Getting Started Guide');
-DELETE FROM users WHERE email = 'admin@example.com';
